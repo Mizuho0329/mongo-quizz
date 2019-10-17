ada005550fc268d479d22616b6c1120565a47fd5
yannik <yguillodo@gmail.com>
2019-10-17 16:53:11 +0200

    all route users  et postman config

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,33 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/UsersQuizz');
+const ObjectId = require('mongoose').Types.ObjectId;
 
-/* GET listing usersQuizz*/
+/* Lister les users de mongoQuizz*/
 router.get('/', function(req, res, next) {
   User.find().limit(10).exec((err, users) => res.json(users));
 });
 
-/* GET 1 user  */
+/* Selectionner un user  */
 router.get('/:id', (req, res) => {
   User.findById(req.params.id).exec((err,sta) => res.json(sta));
 });
 
-/* post 1 user */
+/* Ajouter 1 user */
 router.post('/', (req, res) =>{
   const  user = new User(req.body);
   user.save((err,user) => res.json(user))
-
 });
 
+/* mettre à jour un user*/
+router.put('/:id', (req, res) => {
+  User.updateOne({ _id: ObjectId(req.params.id) }, req.body, (err, user) => res.json(user))
+});
 
-
-
+/* Supprimer un user*/
+router.delete('/:id', (req, res) => {
+  User.findByIdAndDelete(req.params.id, (err, user) => res.json(user))
+});
 
 
 module.exports = router;
